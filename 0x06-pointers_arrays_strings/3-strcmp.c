6fc2b1d0a3ab80641896a208f1a497b2bcf90a0c
temidee01 <benjamin.temidee@gmail.com>
2023-07-26 11:46:12 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,6 +7,7 @@
  * Return: If str1 < str2, the negative difference of the first unmatched characters.
  * If str1 == str2, 0.
  * If str1 > str2, the positive difference of the first unmatched charaters.
+ *
  */
 
 int _strcmp(char *s1, char *s2)
@@ -14,8 +15,8 @@ int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2 && *s1 == *s2)
 	{
-		s1++;
-		s2++;
+	s1++;
+	s2++;
 	}
 	return (*s1 - *s2);
 }
