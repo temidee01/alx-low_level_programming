df72ff54b0fb44cf13c9525eddfac8f408256e3e
temidee01 <benjamin.temidee@gmail.com>
2023-07-26 11:35:02 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,7 +7,6 @@
  * Return: If str1 < str2, the negative difference of the first unmatched characters.
  * If str1 == str2, 0.
  * If str1 > str2, the positive difference of the first unmatched charaters.
- *
  */
 
 int _strcmp(char *s1, char *s2)
