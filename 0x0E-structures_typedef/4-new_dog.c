3dbe2bb9dac33452737fe880ba7c620addc70a35
temidee01 <benjamin.temidee@gmail.com>
2023-08-14 08:30:56 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,3 +23,28 @@ char *_strdup(char *str)
 		ret[length] = *--str;
 	return (ret);
 }
+
+
+/**
+ * new_dog - instantiates a dawg
+ * @name: the dawgy name
+ * @age: the dawgy age
+ * @owner: the dawgy owner
+ *
+ * Return: pointer to new dawg.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d = malloc(sizeof(dog_t));
+
+	if (!d)
+		return (NULL);
+	d->name = _strdup(name);
+	if (name && !d->name)
+		return (free(d), NULL);
+	d->owner = _strdup(owner);
+	if (owner && !d->owner)
+		return (free(d->name), free(d), NULL);
+	d->age = age;
+	return (d);
+}
