07144adedcd48b4f492931efe9d7d6f573988b3d
temidee01 <benjamin.temidee@gmail.com>
2023-08-24 15:43:21 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - length of the list
+ * @h: Pointer to the first node
+ * Return: number of nodes in the list
+ **/
+
+size_t list_len(const list_t *h)
+{
+	int i;
+
+	if (h == NULL)
+		return (0);
+
+	for (i = 1; h->next != NULL; i++)
+		h = h->next;
+	return (i);
+}
